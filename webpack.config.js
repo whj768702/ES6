3aa9c250b76e14ed6a864decff065443371149c9
w0282 <wuhj@dtdream.com>
2017-03-08 10:06:53 +0800

    配置webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 var path = require('path');
- var webpack = require('webpack');
+var webpack = require('webpack');
 
- module.exports = {
-     entry: './js/main.js',
-     output: {
-         path: path.resolve(__dirname, 'build'),
-         filename: 'main.bundle.js'
-     },
-     module: {
-         loaders: [
-             {
-                 test: /\.js$/,
-                 loader: 'babel-loader',
-                 query: {
-                     presets: ['es2015']
-                 }
-             }
-         ]
-     },
-     stats: {
-         colors: true
-     },
-     devtool: 'source-map'
- };
+module.exports = {
+    entry: './js/main.js',
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
